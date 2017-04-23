12dbb70007d6f32fb7d7c9f62528a7f28e4fed28
DESKTOP-I5E7U99\Lasantha <lasantha@cxagroup.com>
2017-04-24 00:59:14 +0800

    libraries

---
+++ app/components/projects/projects.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-dashboard',
+    templateUrl: 'app/components/projects/projects.component.html',
+    //styleUrls: ["app/components/dashboard/projects.component.css"]
+})
+
+export class ProjectsComponent {}
